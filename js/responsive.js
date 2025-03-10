6a38ed63808931428d499f083011073c0ec079cc
Cservenák Dániel <dastavok@freemail.hu>
2025-03-10 18:10:17 +0100

    first commit

---
+++ js/responsive.js
@@ -0,0 +1,10 @@
+function adjustSearchBarMaxLength() {
+    const searchBar = document.querySelector('.search-bar');
+    if (searchBar) {
+        const width = searchBar.clientWidth;
+        searchBar.maxLength = Math.floor(width / 10); // Example calculation
+    }
+}
+
+window.addEventListener('resize', adjustSearchBarMaxLength);
+window.addEventListener('load', adjustSearchBarMaxLength);
