91b39267e6a79067d335ce9ae319df772aa19986
dlt008 <cs.daniel243@gmail.com>
2025-03-27 13:01:37 +0100

    Dark-Light mode data-tooltip change script

--- js/iconChange.js
+++ js/iconChange.js
@@ -1,51 +1,44 @@
-/*var image = document.getElementById("themeBttn");
-
-function changeImage()
-{
-    if (image.getAttribute('src') === '/whiteNavSVG/light mode.svg')
-    {
-        image.src = '/Icons/Black Navbar Icons/dark mode.svg';
-    }
-    else
-    {
-        image.src = '/whiteNavSVG/light mode.svg';
-    }
-}*/
-
 let isDarkMode = false;
 
-// Toggle theme and CSS (if cssToggle() is defined elsewhere)
 function toggleTheme() {
     isDarkMode = !isDarkMode;
     updateIcons();
+    updateTooltip();
+    cssToggle();
 }
+
 
 function updateIcons() {
     const icons = document.querySelectorAll(".theme-icon");
     
     icons.forEach(icon => {
         const currentSrc = icon.src;
-        const iconName = currentSrc.split("/").pop(); // "home.svg", "person.svg", etc.
+        const iconName = currentSrc.split("/").pop();
         
-        // Toggle between "Black Navbar Icons" and "White Navbar Icons" (adjust if needed)
         const newPath = isDarkMode 
-            ? `Icons/White Navbar Icons/${iconName}`  // Dark mode icons
-            : `Icons/Black Navbar Icons/${iconName}`;  // Light mode icons
+            ? `Icons/White Navbar Icons/${iconName}`
+            : `Icons/Black Navbar Icons/${iconName}`;
         
         icon.src = newPath;
     });
 }
 
-function cssToggle() {
-    // Select the <link> element
-    let theme = document.getElementById('theme');
+function updateTooltip() {
+    const themeToggle = document.querySelector('span[data-tooltip][onclick*="toggleTheme"]');
+    
+    if (themeToggle) {
+        themeToggle.setAttribute('data-tooltip', isDarkMode ? 'Light Mode' : 'Dark Mode');
 
-    // Toggle between light.css and dark.css
-    if (theme.getAttribute('href') == 'css/light-mode css/light-mode home.css') {
+        if (typeof refreshTooltips === 'function') {
+            refreshTooltips();
+        }
+    }
+}
+function cssToggle() {
+    const theme = document.getElementById('theme');
+    if (theme.getAttribute('href').includes('light-mode')) {
         theme.setAttribute('href', 'css/dark-mode css/dark-mode home.css');
     } else {
         theme.setAttribute('href', 'css/light-mode css/light-mode home.css');
     }
 }
-
-console.log("Fut a css")
