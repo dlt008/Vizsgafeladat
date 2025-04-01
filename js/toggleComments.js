a108aee98bd601dd53f41dd1faac982d58c8d479
dlt008 <cs.daniel243@gmail.com>
2025-04-01 12:18:55 +0200

    commentSection div átírása, plusz ikonok hozzáadása

--- js/toggleComments.js
+++ js/toggleComments.js
@@ -2,12 +2,26 @@ document.addEventListener("DOMContentLoaded", function () {
     const commentToggles = document.querySelectorAll(".comments-toggle");
 
     commentToggles.forEach(button => {
+        const downArrow = document.createElement("img");
+        downArrow.src = "Icons/Black Navbar Icons/arrowdown.svg";
+        downArrow.alt = "Down Arrow";
+        button.insertBefore(downArrow, button.firstChild);
+
         button.addEventListener("click", function () {
             const commentsSection = this.closest(".Post").querySelector(".Comments");
             commentsSection.classList.toggle("expanded");
 
-            // Change button text dynamically
-            this.textContent = commentsSection.classList.contains("expanded") ? "Close Comments" : "Open Comments";
+            const existingIcon = this.querySelector("img");
+            if (existingIcon) {
+                existingIcon.remove();
+            }
+
+            const newIcon = document.createElement("img");
+            newIcon.alt = commentsSection.classList.contains("expanded") ? "Down Arrow" : "Up Arrow";
+            newIcon.src = commentsSection.classList.contains("expanded") 
+                ? "Icons/Black Navbar Icons/arrowdown.svg"
+                : "Icons/Black Navbar Icons/arrowup.svg";
+            this.insertBefore(newIcon, this.firstChild);
         });
     });
 });
